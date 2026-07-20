ce884c7a1c50eeaf870ba433dd5bd0f82366aa7b
Stéphane Klein <contact@stephane-klein.info>
2026-07-20 12:57:21 +0000

    Center today input in day view

--- src/lib/components/DateInput.svelte
+++ src/lib/components/DateInput.svelte
@@ -56,7 +56,7 @@
     }
 </script>
 
-<div class="inline-flex items-center gap-1">
+<div class="relative w-36 mx-auto">
     <input
         type="text"
         bind:value
@@ -74,7 +74,7 @@
         onclick={openPicker}
         aria-label="Open calendar"
         tabindex="-1"
-        class="cursor-pointer bg-transparent border-none p-0 text-gray-500 hover:text-gray-700"
+        class="absolute -right-[30px] top-1/2 -translate-y-1/2 cursor-pointer bg-transparent border-none p-1 text-gray-500 hover:text-gray-700"
     >
         <svg
             xmlns="http://www.w3.org/2000/svg"
